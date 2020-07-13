2ac4268dd655dab9bf27408a4b954d39881450b3
sabarehmanciit <sabarehmanciit@gmail.com>
2020-07-13 22:59:30 +0500

    First Commit

---
+++ Hummingbirds/Assets/Hummingbird/Scripts/sceneManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour {
+
+	public void loadScene(string str)
+	{
+		SceneManager.LoadScene (str); 
+		 
+	}
+}
